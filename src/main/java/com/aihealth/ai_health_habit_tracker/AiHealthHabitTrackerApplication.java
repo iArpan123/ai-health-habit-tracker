54e2fca336d57e2dc1f3fdbea795832f27e2d200
iarpan123 <arpandeepsingh01@gmail.com>
2025-10-02 18:13:42 -0700

    chore: add Spring Boot starter project with README and configs

---
+++ src/main/java/com/aihealth/ai_health_habit_tracker/AiHealthHabitTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.aihealth.ai_health_habit_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiHealthHabitTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiHealthHabitTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aihealth/ai_health_habit_tracker/AiHealthHabitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aihealth.ai_health_habit_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiHealthHabitTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
